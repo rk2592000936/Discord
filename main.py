9d147d53e8ead0ca0f10dbfcdf099821a6915153
rk2592000936 <50093450+rk2592000936@users.noreply.github.com>
2020-03-11 18:29:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ from discord.ext import commands
 
 bot = commands.Bot()
 
+
 @bot.event
 async def on_ready():
     print('Bot has been online')
+
 
 @bot.event
 async def on_member_join(member):
     channel = bot.get_channel(687239571007864832)
     await channel.send('{}join!'.format(member))
+
 
 @bot.event
 async def on_member_remove(member):
@@ -18,7 +21,9 @@ async def on_member_remove(member):
     await channel.send('{}leave!'.format(member))
 
 # 'ping'就是用于控制bot回复的指令名称，可使用任意名称
-#ctx包含了使用者，id，所在服务器，所在频道等信息，用于提示bot在哪里进行回复
+# ctx包含了使用者，id，所在服务器，所在频道等信息，用于提示bot在哪里进行回复
 @bot.command()
 async def ping(ctx):
     await ctx.send('hello')
+
+bot.run('Njg2MjQ4MDc5NzExNTM1MTQ2.XmUc3g.k_de2UrgbcMFSMTCNFmZbf4DnQE')
